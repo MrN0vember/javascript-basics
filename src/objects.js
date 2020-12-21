e519d31f2817ec5de866312ba245303067dafb52
James <j.a.pratt@outlook.com>
2020-12-21 21:54:13 +0000

    objects 75% complete

--- src/objects.js
+++ src/objects.js
@@ -1,37 +1,38 @@
 const createPerson = (name, age) => {
-  // your code here
+ return {name: name, age: age};
+
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+return property in object;
 };
 
 const isOver65 = person => {
-  // your code here
+ return person.age > 65 ;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(people => people.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+ return people.find(people => people.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+return cars.filter(cars => cars.manufacturer === "Honda");
 };
 
 const averageAge = people => {
-  // your code here
+  
 };
 
 const createTalkingPerson = (name, age) => {
